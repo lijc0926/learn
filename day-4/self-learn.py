fbaaa44e0f623223e3fbe60ecb8ee8ad3d6141b6
lijc0926 <15392597909@163.com>
2020-04-13 22:37:34 +0800

    最新一次提交

--- day-4/self-learn.py
+++ day-4/self-learn.py
@@ -1,13 +1,13 @@
 #coding:utf-8
 class CLanguage :
     # 下面定义了2个类变量
-    def __init__(self,name):
+    name = 'xiaoming'
+    def __init__(self):
         #下面定义 2 个实例变量
-        self.name = name
-        print(name)
+        print(self.name)
     # 下面定义了一个say实例方法
     def say(self,name):
         print(self,name)
 if __name__ == '__main__':
-    a = CLanguage('ljc')
-    a.say('小明')
+    a = CLanguage()
+    a.say('ljc')
